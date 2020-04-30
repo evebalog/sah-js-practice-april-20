5a4987e27fcc990353fd5aaee5634eb6d6b1dc68
evebalog <evebalog@netscape.net>
2020-04-30 17:32:14 +0200

    Uploading practice files

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log('Scripts file is loaded.');
+
+// Single line comment
+/* Multiple
+    lines
+    comment
+*/
+
+// $('css selector').action()
+
+$('h1').text('This is done by JavaScript!');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
+
+   
